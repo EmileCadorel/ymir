1d23ff12391a16a8391428a8733237e86517d8f2
Emile Cadorel <ecadorel@gmail.com>
2017-02-23 14:10:29 +0100

    merge

--- source/semantic/types/StructInfo.d
+++ source/semantic/types/StructInfo.d
@@ -16,28 +16,56 @@ import semantic.types.BoolUtils, semantic.types.RefInfo;
 */
 class StructCstInfo : InfoType {
 
+    /** Le nom de la structure */
     private string _name;
+
+    /** Le parametre de la structure */    
     private Array!TypedVar _params;
+
+    /** Les types des parametres de la structure*/
     private Array!InfoType _types;
+
+    /** Le nom des parametres*/
     private Array!string _names;
+
+    /** La structure a été importé ?*/
     private bool _extern;
     
     this (string name) {
 	this._name = name;
     }
 
+    /**
+     Returns: la structure vient d'un import ?
+    */
     ref bool isExtern () {
 	return this._extern;
     }
-    
+
+    /** 
+     Ajoute un attributs à la structure
+     Params:
+     type = l'attribut (doit avoir été analysé sémantiquement)
+     */
     void addAttrib (TypedVar type) {
 	this._params.insertBack (type);
     }
 
+    /**
+     Returns: le nom de la structure
+     */
     string name () {
 	return this._name;
     }
-    
+
+    /**
+     Instancie un objet crée a partir d'un patron de structure
+     Params:
+     name = le nom de la structure à créer
+     templates = les paramètre templates passé au constructeur
+     Throws: NotATemplate
+     Returns: une instance de StructInfo
+     */
     static InfoType create (Word name, Expression [] templates) {
 	auto cst = cast(StructCstInfo) (Table.instance.get (name.str).type);
 	if (cst is null) assert (false, "Nooooon !!!");
@@ -65,19 +93,34 @@ class StructCstInfo : InfoType {
 	ret.isExtern = cst._extern;
 	return ret;
     }
-    
+
+    /**
+     Returns: false
+     */
     override bool isSame (InfoType) {
 	return false;
     }
-    
+
+    /**
+     Returns: this;
+     */
     override InfoType clone () {
 	return this;
     }
 
+    /**
+     Assert: Impossible de se retrouver la.
+     */
     override InfoType cloneForParam () {
 	assert (false, "constructeur de structure en param !?!");
     }
 
+    /**
+     On utilise la constructeur de la structure pour générer un instance 
+     Params:
+     token = le constructeur
+     params = les paramètres passé à la structure
+     */
     override ApplicationScore CallOp (Word token, ParamList params) {
 	if (params.params.length != this._params.length) {
 	    return null;
@@ -111,6 +154,9 @@ class StructCstInfo : InfoType {
 	return score;
     }
 
+    /**
+     Returns: le nom pour le mangling
+     */
     override string simpleTypeString () {
 	if (this._name [0] >= 'a' && this._name [0] <= 'z') {
 	    return "_" ~ this.name;
@@ -118,6 +164,9 @@ class StructCstInfo : InfoType {
 	    return this._name;
     }    
 
+    /**
+     Returns: le nom complet pour les informations d'erreur
+     */
     override string typeString () {
 	auto name = this._name ~ "(";
 	if (this._types.empty) {	    
@@ -145,10 +194,16 @@ class StructCstInfo : InfoType {
 	return name;
     }
 
+    /**
+     Returns: la taille (même si ca veut rien dire)
+     */
     override LSize size () {
 	return LSize.LONG;
     }
 
+    /**
+     Supprime le constructeur de la table des structures.
+     */
     override void quit (string) {
 	InfoType.removeCreator (this._name);
     }
@@ -156,11 +211,21 @@ class StructCstInfo : InfoType {
     
 }
 
+/**
+ Une instance construite d'une structure
+ */
 class StructInfo : InfoType {
 
+    /** Les types des paramètres de la structure */    
     private Array!InfoType _params;
+
+    /** Les noms de attributs de la structure */
     private Array!string _attribs;
+
+    /** Le nom de la structure */    
     private string _name;
+
+    /** La structure a été importé */
     private bool _extern;
 
     private this (string name, Array!string names, Array!InfoType params) {
@@ -170,36 +235,68 @@ class StructInfo : InfoType {
 	this._destruct = &StructUtils.InstDestruct;
     }
 
+    /**
+     Returns: la structure a été importé
+     */
     ref bool isExtern () {
 	return this._extern;
     }
-    
+
+    /**
+     Créé une instance de la structure
+     Params:
+     name = le nom de la structure
+     names = les noms des attributs
+     params = les types des attributs
+     */
     static InfoType create (string name, Array!string names, Array!InfoType params) {
 	return new StructInfo (name, names, params);
     }
 
-
+    
+    /**
+     Returns: les types des attributs de la structure
+     */
     Array!InfoType params () {
 	return this._params;
     }
 
-    
+    /**
+     Returns: le nom de la structure
+     */    
     string name () {
 	return this._name;
     }    
 
+    /**
+     Surcharge des operateur binaire sur une structure
+     Params: 
+     token = l'operateur
+     right = l'operande droite
+     */
     override InfoType BinaryOp (Word token, Expression right) {
 	if (token == Tokens.EQUAL) return Affect (right);
 	if (token == Keys.IS) return Is (right);
 	else if (token == Keys.NOT_IS) return NotIs (right);
 	return null;
     }
-    
-    override InfoType BinaryOpRight (Word token, Expression right) {
-	if (token == Tokens.EQUAL) return AffectRight (right);
+
+    /**
+     Surcharge des operateurs binaire à droite.
+     Params:
+     token = l'operateur
+     left = l'operande gauche
+     */
+    override InfoType BinaryOpRight (Word token, Expression left) {
+	if (token == Tokens.EQUAL) return AffectRight (left);
 	return null;
     }
 
+    /**
+     Surcharge de l'operateur 'is'.
+     Sur une structure is permet de verifier que le structure n'est pas null ou de verifier son type
+     Returns: une instance de BoolInfo, ou null
+     */
     private InfoType Is (Expression right) {
 	if (this.isSame (right.info.type)) {
 	    auto b = new BoolInfo ();
@@ -218,6 +315,11 @@ class StructInfo : InfoType {
 	return null;
     }    
 
+    /**
+     Surcharge de l'operateur '!is'.
+     Cf: Is
+     Returns: une instance de BoolInfo, ou null
+     */
     private InfoType NotIs (Expression right) {
 	if (this.isSame (right.info.type)) {
 	    auto b = new BoolInfo ();
@@ -235,7 +337,11 @@ class StructInfo : InfoType {
 	}
 	return null;
     }    
-    
+
+    /**
+     Surcharge de l'operateur '='.
+     Returns: une instance de StructInfo ou null
+     */
     private InfoType Affect (Expression right) {
 	auto _st = cast (StructInfo) right.info.type;
 	if (_st && _st.name == this._name) {
@@ -249,7 +355,11 @@ class StructInfo : InfoType {
 	}
 	return null;
     }
-    
+
+    /**
+     Surcharge de l'operateur '=' à droite.
+     Returns: une instance de StructInfo, ou null
+     */
     private InfoType AffectRight (Expression left) {
 	if (cast (UndefInfo) left.info.type) {
 	    auto other = this.clone ();
@@ -258,7 +368,12 @@ class StructInfo : InfoType {
 	}
 	return null;
     }    
-        
+
+    /**
+     Surcharge de l'operateur '.'
+     StructInfo surcharge 'init' et 'typeid' comme propriété.
+     On peut aussi acceder au attributs de cette manière.
+     */
     override InfoType DotOp (Var var) {
 	if (var.token.str == "init") return Init ();
 	else if (var.token.str == "typeid") return StringOf ();
@@ -272,6 +387,9 @@ class StructInfo : InfoType {
 	return null;
     }
 
+    /**
+     Surcharge du cast automatique     
+     */
     override InfoType CompOp (InfoType other) {
 	if (cast (UndefInfo) other || this.isSame (other)) {
 	    auto ret = this.clone ();
@@ -286,25 +404,39 @@ class StructInfo : InfoType {
 	}
 	return null;
     }
-    
+
+    /**
+     Traitement à appliquer quand on passe le structure en paramètre.
+     Returns: le type contenant le traitement.
+     */
     override InfoType ParamOp () {
 	auto ret = this.clone ();
 	ret.lintInstS.insertBack (&ClassUtils.InstParam);
 	return ret;
     }
 
+    /**
+     Traitement à appliquer quand on retourne la structure
+     Returns: le type contenant le traitement.
+     */
     override InfoType ReturnOp () {
 	auto ret = this.clone ();
 	ret.lintInstS.insertBack (&ClassUtils.InstReturn);
 	return ret;
     }    
-    
+
+    /**
+     Valeur d'init d'une structure
+     */
     private InfoType Init () {
 	auto t = this.clone ();
 	t.lintInst = &StructUtils.Init;
 	return t;
     }
 
+    /**
+     surcharge de la propriété typeid.
+     */
     private InfoType StringOf () {
 	auto str = new StringInfo;
 	str.lintInst = &StructUtils.StringOf;
@@ -312,6 +444,9 @@ class StructInfo : InfoType {
 	return str;
     }
 
+    /**
+     Accés à un paramètre de la structure.
+     */
     private InfoType GetAttrib (ulong nb) {
 	auto type = this._params [nb].clone ();
 	if (auto _cst = cast (StructCstInfo) type) {
@@ -326,7 +461,10 @@ class StructInfo : InfoType {
 	type.setDestruct (null);
 	return type;
     }    
-    
+
+    /**
+     Les deux types sont ils identique ?
+     */
     override bool isSame (InfoType other) {
 	auto type = cast (StructInfo) other;
 	if (type && type._name == this._name) {
@@ -334,7 +472,10 @@ class StructInfo : InfoType {
 	}
 	return false;
     }
-    
+
+    /**
+     Returns: le nom complet pour les informations d'erreur.
+     */
     override string typeString () {
 	auto name = this._name ~ "(";
 	foreach (it ; this._params) {
@@ -350,23 +491,35 @@ class StructInfo : InfoType {
 	return name;
     }
 
+    /**
+     Returns: le nom pour le mangling.
+     */
     override string simpleTypeString () {
 	if (this._name [0] >= 'a' && this._name [0] <= 'z') {
 	    return "_" ~ this.name;
 	} else 
 	    return this._name;
     }    
-    
+
+    /**
+     Returns: une nouvelle instance de StructInfo, avec les informations de destruction concervées.
+     */
     override InfoType clone () {
 	auto ret = create (this._name, this._attribs, this._params);
 	if (this._destruct is null) ret.setDestruct (null);
 	return ret;
     }
-    
+
+    /**
+     Returns: une nouvelle instance de StructInfo, avec les informations de destruction remise à zero.
+    */
     override InfoType cloneForParam () {
 	return create (this._name, this._attribs, this._params);
     }
 
+    /**
+     Returns: les informations de destruction du type.
+     */
     override InfoType destruct () {
 	if (this._destruct is null) return null;
 	auto ret = this.clone ();
@@ -374,9 +527,11 @@ class StructInfo : InfoType {
 	return ret;
     }
 
+    /**
+     Returns: la taille en mémoire du type.
+     */
     override LSize size () {
-	return LSize.LONG;
+	return LSize.ULONG;
     }
-    
-
+   
 }
