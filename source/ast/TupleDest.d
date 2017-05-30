8a86718de2c47410aebfc522da1b2dd0788a675b
Emile Cadorel <ecadorel@gmail.com>
2017-05-30 13:08:50 +0200

    Destruction de tuple également possible avec un système variadics

--- source/ast/TupleDest.d
+++ source/ast/TupleDest.d
@@ -6,7 +6,7 @@ import std.stdio, std.string, std.outbuffer;
 import semantic.types.UndefInfo, utils.exception;
 import ast.Binary, ast.Expand;
 import semantic.types.TupleInfo;
-import syntax.Tokens;
+import syntax.Tokens, ast.Tuple;
 
 class TupleDest : Instruction {
 
@@ -43,28 +43,39 @@ class TupleDest : Instruction {
 	if (!cast (TupleInfo) right.info.type) throw new DestOfNonTuple (right.info);
 	auto tupleType = cast (TupleInfo) right.info.type;
 	auto aff = Word (this._token.locus, Tokens.EQUAL.descr, true);
-	Array!Expression insts;
-	if (!this._isVariadic) {	    
-	    if (this._decls.length != tupleType.params.length)
-		throw new DestOfNonTuple (this._decls.length, tupleType.params.length, right.info);
+	Array!Expression insts;   
+	if (!this._isVariadic && this._decls.length != tupleType.params.length)
+	    throw new DestOfNonTuple (this._decls.length, tupleType.params.length, right.info);
+	else if (this._isVariadic && this._decls.length > tupleType.params.length - 1)
+	    throw new DestOfNonTuple (this._decls.length, tupleType.params.length, right.info);
 
-	    ulong i = 0;
-	    foreach (it ; this._decls []) {
-		auto aux = new Var (it.token);
-		auto info = Table.instance.get (it.token.str);		
-		if (info !is null && Table.instance.sameFrame (info)) throw new ShadowingVar (it.token, info.sym);
-		aux.info = new Symbol (aux.token, new UndefInfo (), false);
-		Table.instance.insert (aux.info);
+	ulong i = 0;
+	foreach (it ; this._decls []) {
+	    auto aux = new Var (it.token);
+	    auto info = Table.instance.get (it.token.str);		
+	    if (info !is null && Table.instance.sameFrame (info)) throw new ShadowingVar (it.token, info.sym);
+	    aux.info = new Symbol (aux.token, new UndefInfo (), false);
+	    Table.instance.insert (aux.info);
+	    if (i == this._decls.length - 1 && this._isVariadic) {
+		Array!Expression last;	    
+		foreach (it_ ; this._decls.length - 1 .. tupleType.params.length) {
+		    auto exp = new Expand (right.token, right, it_);
+		    exp.info = new Symbol (false, exp.token, tupleType.params [it_].clone);
+		    last.insertBack (exp);
+		}
+	    
+		auto ctuple = new ConstTuple (right.token, right.token, last);
+		insts.insertBack (new Binary (aff, new Var (this._decls [$ - 1].token), ctuple).expression);
+	    } else {		
 		auto exp = new Expand (right.token, right, i);
 		exp.info = new Symbol (false, exp.token, tupleType.params [i].clone);
-		
+		    
 		insts.insertBack (new Binary (aff, new Var (it.token), exp).expression);
 		i ++;
 	    }
+	}
 	    
-	    return new TupleDest (this._token, insts, right);
-	} else
-	    assert (false, "TODO");
+	return new TupleDest (this._token, insts, right);
     }
 
     Expression expr () {
