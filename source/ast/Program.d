35515369a9e17b3a0450484ebe7ef0425fa2d00f
Emile Cadorel <ecadorel@gmail.com>
2017-05-31 14:54:18 +0200

    stdio/print est devenue un fichier core (pas besoin d'importer)

--- source/ast/Program.d
+++ source/ast/Program.d
@@ -20,7 +20,8 @@ class Program {
     this (Word token, Array!Declaration decls) {
 	__declareAtBegins__ = [
 			       Word (Location (0, 0, 0, "core/int.yr"), "core/int", false),
-			       Word (Location (0, 0, 0, "core/string.yr"), "core/string", false)
+			       Word (Location (0, 0, 0, "core/string.yr"), "core/string", false),
+			       Word (Location (0, 0, 0, "core/stdio.yr"), "core/stdio", false)
 	];
 	this._locus = token;
 	this._decls = decls;
